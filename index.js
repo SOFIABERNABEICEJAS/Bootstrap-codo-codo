191f34bf93b93cb71de28bd6912c4b557c1b0451
unknown <sofiabernabeicejas@gmail.com>
2022-10-23 17:26:42 -0300

    dom js

--- index.js
+++ index.js
@@ -2,8 +2,32 @@
 
 const botonComprarTickets = document.getElementById("boton-comprar-tickets");
 const seccionConferencia = document.getElementById("seccion-conferencia");
+const seccionComprar = document.getElementById("seccion-comprar")
+const seccionLugar = document.getElementById("seccion-lugar")
+const seccionOradores =document.getElementById("seccion-oradores")
+const seccionConvertite = document.getElementById("seccion-convertite")
+
+const inputCantidad= document.getElementById("input-cantidad")
 //Funcion Onclick
 
 botonComprarTickets.onclick = () => {
-	seccionConferencia.classList.add("none");
+	seccionConferencia.classList.add("ocultar");
+	seccionLugar.classList.add("d-none");
+	seccionOradores.classList.add("d-none");
+	seccionConvertite.classList.add("d-none");
+	seccionComprar.classList.remove("d-none")
 };
+
+// Funcion sumar 
+
+
+
+    //seccionConvertite.innerHTML = "<em>Total a Pagar FUNCIONAAAA: $"+inputCantidad.value+"</em>";
+    
+
+
+
+
+console.log(inputCantidad.value)
+
+
